e1f4b2779a324854de1193f4d171c8ea2c6389ab
trantienvn <97656011+trantienvn@users.noreply.github.com>
2024-02-26 22:02:47 -0800

    load

--- api.js
+++ api.js
@@ -3,19 +3,22 @@ const password = '';
 const currentUrl = new URL(window.location.href);
 const id = currentUrl.searchParams.get("id");
 
-fetch(`${apiUrl}?password=${password}&id=${id}`)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Có lỗi khi tải dữ liệu từ API');
-    }
-    return response.json();
-  })
-  .then(data => {
+
+$.ajax({
+  url: 'http://api.trantien.free.nf/ajax.php',
+  type: 'GET',
+  data: {
+    password: password,
+    id: id
+  },
+  success: function(data) {
+    console.log('Dữ liệu từ API:', data);
     const jsonObject = JSON.parse(data);
     console.log('Dữ liệu từ API:', data);
     const myParagraph = document.getElementById('noidung');
     myParagraph.innerText = jsonObject.noidung;
-  })
-  .catch(error => {
-    console.error(error.message);
-  });
+  },
+  error: function(error) {
+    console.error('Đã có lỗi khi tải dữ liệu từ API:', error);
+  }
+});
